fde760f4e71aeee1ee1b0c8f33013d8535474c5b
owis <bcabezasr@gmail.com>
2026-01-22 12:42:06 -0300

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,7 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import react from "@astrojs/react"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  vite: {
-    define: {
-      "import.meta.env.PUBLIC_API_URL": JSON.stringify(process.env.PUBLIC_API_URL || "http://localhost:3001"),
-      "import.meta.env.PUBLIC_WS_URL": JSON.stringify(process.env.PUBLIC_WS_URL || "ws://localhost:3001"),
-    },
-  },
-})
+});
