8250b9fa54c833da96ac8a61ad47bc382770c16f
owis <bcabezasr@gmail.com>
2026-01-22 18:37:23 -0300

    subir tamaño limite imagen

--- backend/server.js
+++ backend/server.js
@@ -78,8 +78,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Static files
 app.use("/uploads", express.static(join(__dirname, "public/uploads")));
