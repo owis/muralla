66bf8272439d1ab48e2a1ddac08c50b7340f3562
owis <bcabezasr@gmail.com>
2026-01-22 13:22:58 -0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "",
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -21,7 +22,9 @@ export async function testConnection() {
     connection.release();
     return true;
   } catch (error) {
-    console.error("Database connection failed:", error.message);
+    console.error(
+      `ERROR DB connection - ${process.env.DB_HOST}@${process.env.DB_USER}: ${error.message}`,
+    );
     return false;
   }
 }
